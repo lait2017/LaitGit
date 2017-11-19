5b51f8d6ae9df19f02de5a9677661b013741c0ea
Robert Lajt <r.haniewski@lait.pl>
2017-11-19 12:52:31 +0100

    polskie znaki

--- LaitProjekt/src/laitJava/Tomasz.java
+++ LaitProjekt/src/laitJava/Tomasz.java
@@ -51,9 +51,9 @@ System.out.print("Stopnie Celcjusza przeliczone na Farenhaita " + (1.8 * liczba 
 		String string = "napis1";
 		char z = 'z';
 		double zmiennoprzecinkowa = 3.0;
-		boolean prawdaFa³sz = false;
+		boolean prawdaFalsz = false;
 
-		if (prawdaFa³sz) {
+		if (prawdaFalsz) {
 			System.out.println("jestem w ifie" + zmiennanr1);
 			zmiennanr1++;
 		}
