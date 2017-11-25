1184e5b5a71cbb883e97f3a2f22b525770279558
pieczorin1 <basia34@interia.pl>
2017-11-25 12:48:03 +0100

    kommit sql c.d.

--- basiaConnection.sql
+++ basiaConnection.sql
@@ -1,8 +1,38 @@
-select* 
-from COUNTRIES;
+insert into COUNTRIES (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+values ('PL', 'POLAND', 1);
 
-select country_name
-from COUNTRIES;
+SELECT * FROM COUNTRIES
+
+insert into COUNTRIES (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+values ('SP', 'SPAIN', 1);
+
+insert into COUNTRIES (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+values ('AT', 'AUSTRIA', 1);
+
+insert into COUNTRIES (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+values ('CL', 'CHILE', 2);
+
+insert into COUNTRIES (COUNTRY_ID, COUNTRY_NAME, REGION_ID)
+values ('PW', 'POLAND', 1);
+
+select distinct country_name from Countries;
+select distinct region_id from Countries;
+
+select*from EMPLOYEES;
+
+select employee_id as "ID pracownika", FIRST_NAME as "Imiê", LAST_NAME as "Nazwisko", phone_number as "Numer telefonu",
+HIRE_DATE as "Data zatrudnienia", JOB_ID as "ID zatrudnienia", salary as "Pensja",
+COMMISSION_PCT as "Prowizja", MANAGER_ID as "ID MENED¯ERA", department_id as "ID departamentu"
+from employees;
+
+select*from  employees where manager_ID=100;
+
+select*from employees where Employee_id>100;
+
+
+
+
+
 
 
 
