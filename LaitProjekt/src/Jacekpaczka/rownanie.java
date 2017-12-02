97e058c2fa191dd0442ca31828d7393a9af40eda
Your Name <you@example.com>
2017-12-02 13:11:20 +0100

    pliki funkcje java

---
+++ LaitProjekt/src/Jacekpaczka/rownanie.java
@@ -0,0 +1,26 @@
+package Jacekpaczka;
+import java.util.Scanner;
+public class rownanie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a,b,c;
+		System.out.println("obliczanie wartosci x w funkcji ax+b=c");
+		Scanner skaner = new Scanner(System.in);
+		System.out.println("podaj wartosc a");
+		a = skaner.nextDouble();
+		System.out.println("podaj wartosc b");
+		b = skaner.nextDouble();
+		System.out.println("podaj wartosc c");
+		c = skaner.nextDouble();
+	
+		
+			System.out.println("x= " + (c-b)/a);
+		
+		
+	}}
+		
+	
+
+
---
+++ LaitProjekt/src/Jacekpaczka/zad7.java
@@ -0,0 +1,19 @@
+package Jacekpaczka;
+
+public class zad7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x;
+		
+		System.out.println("obliczam wartoœæ równania y=3x zmieniaj¹c x od 0 do 10");
+		x=0;
+		do {System.out.println(3*x);
+		x++;}
+		while 
+			(x<10);
+		
+	}
+
+}
