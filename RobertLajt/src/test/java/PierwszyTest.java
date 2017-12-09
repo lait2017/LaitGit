cd6e54f360c1d83eba980bc3e5457f54352f2c0d
Robert Lajt <r.haniewski@lait.pl>
2017-12-09 09:49:13 +0100

    commit

--- RobertLajt/src/test/java/PierwszyTest.java
+++ RobertLajt/src/test/java/PierwszyTest.java
@@ -1,11 +1,15 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -15,15 +19,19 @@ public class PierwszyTest {
         ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://allegro.pl");
-WebElement raty0procent = driver.findElement(By.linkText("Teraz na Allegro Raty 0%"));
-//WebElement raty0 = driver.findElement(By.xpath("//div[@class='bar-wrapper']" +
- //       "/p[@class='message']/a"));
-        assertTrue("");
-raty0procent.click();
-        //raty0.click();
-//        FirefoxDriverManager.getInstance().setup();
-//        WebDriver firefoxDriver = new FirefoxDriver();
-//        firefoxDriver.get("http://google.pl");
-    }
+        //background-color: #e1f1e1
 
+WebElement raty0procent = driver.findElement(By.linkText("Teraz na Allegro Raty 0%"));
+        assertTrue("raty 0 procent jest nie widoczne",raty0procent.isEnabled());
+        assertTrue("brakuje textu Allegro Raty",raty0procent.getText().contains("Allegro Raty"));
+        raty0procent.click();
+
+       List<WebElement> kategorieRat = driver.findElements(By.className("contentArticle__tileItem"));
+       WebElement sportiWypoczynek = kategorieRat.get(3);
+       WebElement wysukiwarka = driver.findElement(By.xpath("//div[contains(@class, 'atag')));
+        assertTrue("brakuje textu Sport i Wypoczynek",sportiWypoczynek.getText().contains("Sport i wypoczynek"));
+sportiWypoczynek.click();
+
+
+    }
 }
